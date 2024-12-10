8f1dc6c6a267b90f188926e76fc0ca614f79f78b
kusum-Malviya <malviyakusum56@gmail.com>
2024-12-10 12:18:50 +0530

    Initialized Project Wonderlust

--- init/index.js
+++ init/index.js
@@ -12,14 +12,14 @@ main()
 
 async function main() {
   // Connection Function
-  await mongoose.connect(dbURl);
+  await mongoose.connect("mongodb+srv://kirtanmalviya49:k7Bh0b1lzSijl8SA@cluster0.tcw49.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
   initData.data = initData.data.map((obj) => ({
     ...obj,
-    owner: "6730a0fa6ac7dc4392ad2092",
+    owner: "6757d92be77a68c3996e05bb",
   }));
   await Listing.insertMany(initData.data);
   console.log("Data was Initialized");
